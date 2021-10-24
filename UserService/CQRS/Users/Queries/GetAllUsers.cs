30aecb27104365a1cbb0edb99feb18b60936c998
fasgetz <fasgetz@yandex.ru>
2021-10-24 05:02:43 +0300

    Разработка микросервиса, который отвечает за пользователей

---
+++ UserService/CQRS/Users/Queries/GetAllUsers.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DataBaseLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserService.CQRS.Users.Queries
+{
+
+    /// <summary>
+    /// Запрос на получение всех юзеров
+    /// </summary>
+    public class GetAllUsers : IRequest<IEnumerable<Account>>
+    {
+        public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsers, IEnumerable<Account>>
+        {
+            private readonly TestDataBaseContext _context;
+
+            public GetAllUsersQueryHandler(TestDataBaseContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IEnumerable<Account>> Handle(GetAllUsers request, CancellationToken cancellationToken)
+            {
+                var users = await _context.Accounts.Include(i => i.Profile.Department).Select(i => new Account() 
+                { 
+                    Id = i.Id, 
+                    Email = i.Email,
+                    Name = i.Name,
+                    Profile = i.Profile != null ? i.Profile : new Profile() { Id = i.Id }
+                }).ToListAsync();
+
+                return users;
+            }
+        }
+    }
+}
---
+++ UserService/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using DataBaseLayer;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.CQRS.Users.Queries;
+
+namespace UserService.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Account>> Get()
+        {
+            IEnumerable<Account> users = await mediator.Send(new GetAllUsers());
+
+            return users;
+        }
+    }
+}
